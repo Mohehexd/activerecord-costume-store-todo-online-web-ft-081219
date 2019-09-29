38abe7c6f827dbf6f4c51c2951d1a1ac33857d9f
Guillermo Martinez <dumblemoe@gmail.com>
2019-09-29 00:22:53 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -4,7 +4,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
     create_table :haunted_houses do |t|
       t.string :name 
       t.string :location
-      t.string :location 
+      t.string :theme 
       t.integer :price 
       t.boolean :family_friendly
       t.datetime :opening_date
